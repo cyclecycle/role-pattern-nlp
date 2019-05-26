ae27dda213a31dfa4ed3339877af4c0224f0f675
Nick Morley <nick.morley111@gmail.com>
2019-05-26 23:10:03 +0200

    Add build_pattern.py example

---
+++ examples/build_pattern.py
@@ -0,0 +1,24 @@
+import en_core_web_sm
+text = "Forging involves the shaping of metal using localized compressive forces."
+nlp = en_core_web_sm.load()
+doc = nlp(text)
+
+from role_pattern_nlp import RolePatternBuilder
+
+match_example = {
+    'arg1': [doc[0]],  # [Forging]
+    'pred': [doc[1]],  # [involves]
+    'arg2': [doc[3]],  # [shaping]
+}
+
+# Create a dictionary of all the features we want our RolePatternBuilder to have access to
+feature_dict = {'DEP': 'dep_', 'TAG': 'tag_'}
+role_pattern_builder = RolePatternBuilder(feature_dict)  # Instantiate the pattern builder
+pattern = role_pattern_builder.build(doc, match_example)  #  Build a pattern. It will use all the features in the feature_dict by default.
+print(pattern)
+
+matches = pattern.match(doc)
+print(matches)
+'''
+[{'arg1': [Forging], 'arg2': [shaping], 'pred': [involves]}]
+'''
