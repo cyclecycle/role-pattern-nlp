4c2e1a3929e0c5f0dc8dc94072e49f811e5c849e
Nick Morley <nick.morley111@gmail.com>
2019-06-19 19:08:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    requirements = fh.readlines()
 
 setuptools.setup(
     name="role-pattern-nlp",
-    version="0.0.3",
+    version="0.0.4",
     author="Nicholas Morley",
     author_email="nick.morley111@gmail.com",
     description=" Build and match patterns for semantic role labelling",
@@ -13,10 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cyclecycle/role-pattern-nlp",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'spacy',
-        'spacy-pattern-builder',
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
